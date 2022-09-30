652540ba12fd52cdc0712be108201ae294851b31
Andrew <a.aguirre@damstratechnology.com>
2022-10-01 07:36:42 +0800

    Implementation using useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import github from "./db.js";
+import { useEffect, useState, useCallback } from "react";
+//useCallBack
+
+import query from "./Query"
+
 function App() {
+  let [userName, setUserName] = useState("");
+
+  useEffect(() => {  
+   
+    fetch(github.baseUrl, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(query),
+    })
+    .then(response => response.json())
+    .then((data) => {
+      setUserName(data.data.viewer.name);
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  });
+  
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill">
           Repos
         </i>
-      </h1>      
+      </h1>
+      <p>Hey there {userName}</p>
     </div>
   );
 }
